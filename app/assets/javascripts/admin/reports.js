46be3627bcc63ad9a0166fcaaa2aa8f2dfca42ec
lequangcanh <lequangcanh94@gmail.com>
2017-02-14 16:57:56 +0700

    Fix css for report venue

---
+++ app/assets/javascripts/admin/reports.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.admin-report-button').click(function() {
+    $('.admin-report-navbar').width('25%');
+  });
+  $('.admin-report-close-button').click(function() {
+    $('.admin-report-navbar').width(0);
+  });
+});
